2fc42a45464c6e36623245d20b34c2e853b3fae4
Dave Ferrara <daveferrara1@gmail.com>
2017-02-23 12:33:27 -0700

    Create Hash file and Multi File Output Options.

--- index.js
+++ index.js
@@ -8,118 +8,147 @@ var Handlebars = require("handlebars");
 var glob = require("glob");
 
 
-// export Handlebars for easy access in helpers
+// Export Handlebars for easy access in helpers.
 HandlebarsPlugin.Handlebars = Handlebars;
 
 
 function getHelperId(filepath) {
-    var id = filepath.match(/\/([^\/]*).js$/).pop();
-    return id.replace(/\.?helper\.?/, "");
+  var id = filepath.match(/\/([^\/]*).js$/).pop();
+  return id.replace(/\.?helper\.?/, "");
 }
 
 
 function addHelper(Handlebars, id, fun) {
-    console.log(chalk.grey("HandlebarsPlugin: registering helper " + id));
-    Handlebars.registerHelper(id, fun);
+  console.log(chalk.grey("HandlebarsPlugin: registering helper " + id));
+  Handlebars.registerHelper(id, fun);
 }
 
 function HandlebarsPlugin(options) {
-    if (options.onBeforeSetup) {
-        options.onBeforeSetup(Handlebars);
+  if (options.onBeforeSetup) {
+    options.onBeforeSetup(Handlebars);
+  }
+  this.options = options;
+  this.outputFile = options.output;
+  this.entryFile = options.entry;
+  this.data = options.data || {};
+  this.fileDependencies = [];
+
+  // Register helpers.
+  var self = this;
+  var helperQueries = options.helpers || {};
+  Object.keys(helperQueries).forEach(function (helperId) {
+    var foundHelpers;
+
+    // Globbed paths.
+    if (typeof helperQueries[helperId] === "string") {
+      foundHelpers = glob.sync(helperQueries[helperId]);
+      foundHelpers.forEach(function (pathToHelper) {
+        addHelper(Handlebars, getHelperId(pathToHelper), require(pathToHelper));
+        self.addDependency(pathToHelper);
+      });
+
+      // Functions.
+    } else {
+      addHelper(Handlebars, helperId, helperQueries[helperId]);
     }
-
-    this.options = options;
-	this.outputFile = options.output;
-	this.entryFile = options.entry;
-	this.data = options.data || {};
-    this.fileDependencies = [];
-
-    // register helpers
-    var self = this;
-    var helperQueries = options.helpers || {};
-    Object.keys(helperQueries).forEach(function (helperId) {
-        var foundHelpers;
-
-        // globbed paths
-        if (typeof helperQueries[helperId] === "string") {
-            foundHelpers = glob.sync(helperQueries[helperId]);
-            foundHelpers.forEach(function (pathToHelper) {
-                addHelper(Handlebars, getHelperId(pathToHelper), require(pathToHelper));
-                self.addDependency(pathToHelper);
-            });
-
-        // functions
-        } else {
-            addHelper(Handlebars, helperId, helperQueries[helperId]);
-        }
-    });
+  });
 }
 
-HandlebarsPlugin.Handlebars = Handlebars;
-
 HandlebarsPlugin.prototype.readFile = function (filepath) {
-    this.fileDependencies.push(filepath);
-    return fs.readFileSync(filepath, "utf-8");
+  this.fileDependencies.push(filepath);
+  return fs.readFileSync(filepath, "utf-8");
 };
 
 HandlebarsPlugin.prototype.addDependency = function () {
-    this.fileDependencies.push.apply(this.fileDependencies, arguments);
+  this.fileDependencies.push.apply(this.fileDependencies, arguments);
 };
 
 HandlebarsPlugin.prototype.apply = function (compiler) {
-    var self = this;
-    var options = this.options;
-    var data = this.data;
-    var entryFile = this.entryFile;
-    var outputFile = this.outputFile;
 
-    compiler.plugin("compile", function (object, done) {
-        var templateContent;
-        var template;
-        var result;
-        var partials;
+  var self = this;
+  var options = this.options;
+  var data = this.data;
 
-        // fetch paths to partials
-        partials = partialUtils.loadMap(Handlebars, options.partials);
+  compiler.plugin("emit", function (compilation, done) {
 
-        if (options.onBeforeAddPartials) {
-            options.onBeforeAddPartials(Handlebars, partials);
-        }
-        // register partials
-        partialUtils.addMap(Handlebars, partials);
-        // watch all partials for changes
-        self.addDependency.apply(self, Object.keys(partials).map(function (key) {return partials[key]; }) );
+    var templateContent;
+    var template;
+    var result;
+    var partials;
+    // Options entries.
+    var entries = options.entries;
 
-        templateContent = self.readFile(entryFile, "utf-8");
+    // Fetch paths to partials.
+    partials = partialUtils.loadMap(Handlebars, options.partials);
+    // Possibly add to partials.
 
-        if (options.onBeforeCompile) {
+    if (options.onBeforeAddPartials) {
+      options.onBeforeAddPartials(Handlebars, partials);
+    }
+
+    // Possibly add [hash] partial.
+    if (options.hash) {
+      partials[partialUtils.getPartialId(options.hash)] = options.hash;
+      fs.outputFileSync(options.hash, compilation.hash, "utf-8");
+      // Log where we created file.
+      console.log(chalk.green(options.hash + " created"));
+    }
+
+    // Register all partials.
+    partialUtils.addMap(Handlebars, partials);
+    // Watch all partials for changes.
+    self.addDependency.apply(self, Object.keys(partials).map(function (key) {
+      return partials[key];
+    }));
+
+    // Create each output file.
+    if (entries) {
+      for (var key in entries) {
+        if (entries.hasOwnProperty(key)) {
+          var entry = entries[key];
+          for (var prop in entry) {
+            if (entry.hasOwnProperty(prop)) {
+              if (prop = 'entry') {
+                var entryFile = entry[prop];
+              }
+              if (prop = 'output') {
+                var outputFile = entry[prop];
+              }
+
+            }
+          }
+
+          templateContent = self.readFile(entryFile, "utf-8");
+
+          if (options.onBeforeCompile) {
             templateContent = options.onBeforeCompile(Handlebars, templateContent) || templateContent;
-        }
-        template = Handlebars.compile(templateContent);
+          }
+          template = Handlebars.compile(templateContent);
 
-        if (options.onBeforeRender) {
+          if (options.onBeforeRender) {
             data = options.onBeforeRender(Handlebars, data) || data;
-        }
-        result = template(data);
+          }
+          result = template(data);
 
-        if (options.onBeforeSave) {
+          if (options.onBeforeSave) {
             result = options.onBeforeSave(Handlebars, result) || result;
-        }
-    	fs.outputFileSync(outputFile, result, "utf-8");
+          }
+          fs.outputFileSync(outputFile, result, "utf-8");
+          // Log where we created file.
+          console.log(chalk.green(outputFile + " created"));
 
-        console.log(chalk.green(outputFile + " created"));
-
-        if (options.onDone) {
+          if (options.onDone) {
             options.onDone(Handlebars);
+          }
         }
-    });
-
-    compiler.plugin("emit", function (compiler, done) {
-        // add dependencies to watch. This might not be the correct place for that - but it works
-        // webpack filters duplicates...
-        compiler.fileDependencies = compiler.fileDependencies.concat(self.fileDependencies);
-        done();
-    });
+      }
+    }
+    // Add dependencies to watch. This might not be the correct place
+    // for that - but it works.
+    // webpack filters duplicates...
+    compilation.fileDependencies = compilation.fileDependencies.concat(self.fileDependencies);
+    done();
+  });
 };
 
 
